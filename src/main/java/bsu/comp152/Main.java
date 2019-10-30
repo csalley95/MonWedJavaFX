84fc404d6059ca542ee7f56359280cde82873733
csalley95 <c1salley@student.bridgew.edu>
2019-10-30 14:53:26 -0400

    completed

--- src/main/java/bsu/comp152/Main.java
+++ src/main/java/bsu/comp152/Main.java
@@ -2,10 +2,12 @@ package bsu.comp152;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Paint;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 import javafx.scene.control.Label;
-
-import java.awt.*;
 
 public class Main extends Application {
 
@@ -15,9 +17,22 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) {
+        VBox box = new VBox();
         var text = new Label("Hello Comp152");
-        var container = new Scene(text);
+        var color = Paint.valueOf("Blue");
+        text.setTextFill(color);
+        var font = Font.font(22);
+        text.setFont(font);
+        box.getChildren().add(text);
+        var container = new Scene(box);
+        Button pushButton = new Button("Push me");
+        pushButton.setFont(font);
+        var color2 = Paint.valueOf("Green");
+        pushButton.setTextFill(color2);
+        box.getChildren().add(pushButton);
         primaryStage.setScene(container);
+        primaryStage.setTitle("Example Window");
+        primaryStage.setMinWidth(300);
         primaryStage.show();
 
     }
